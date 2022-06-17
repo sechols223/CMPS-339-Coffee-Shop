e7d0f61daeffbf7f3aa130b49ab3da137bcd55b7
Samuel Echols <samuel.echols@selu.edu>
2022-06-17 18:41:45 -0500

    redid file structure and added script to build to heroku

---
+++ api/customers.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = Expres.Rotuer()
+
+
+//Gets all customers
+router.get('/api/customers')
+
+//Gets customers by ID
+router.get('/api/customers/:id', (req, res) => {
+
+})
+
+module.exports = router;
