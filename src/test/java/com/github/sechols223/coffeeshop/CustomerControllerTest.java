5a71bbfe283705b1bde4b0b130476656f2e30e50
Samuel Echols <samuel.echols@selu.edu>
2022-07-06 18:15:36 -0500

    updated travis.yml to reflect spring boot

--- src/test/java/com/github/sechols223/coffeeshop/CustomerControllerTest.java
+++ src/test/java/com/github/sechols223/coffeeshop/CustomerControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
